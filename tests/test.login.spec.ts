858c903d639fc662261c3c680df481539ce70453
Harsh <harsh.singal31@gmail.com>
2026-07-04 14:46:51 +1000

    inventory_list_page_object

--- tests/test.login.spec.ts
+++ tests/test.login.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../models/pages/login.page";
+import InventoryListPage from "../models/pages/inventoryList.page";
 
 test("Test Successful login", async ({ page }) => {
   const loginPage = new LoginPage(page);
@@ -11,4 +12,28 @@ test("Test Successful login", async ({ page }) => {
   await loginPage.clickLoginButton();
 
   await expect(page.locator(".inventory_list")).toBeVisible();
+});
+
+test("Check inventory Items Count", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await page.goto("https://www.saucedemo.com/");
+
+  await loginPage.inputUserName("standard_user");
+  await loginPage.inputPassword("secret_sauce");
+  await loginPage.clickLoginButton();
+
+  const inventoryItemsPage = new InventoryListPage(page);
+
+  console.log(await inventoryItemsPage.getInventoryItemsCount());
+  const item = inventoryItemsPage.getInventoryItemByName("Sauce Labs Onesie");
+
+  console.log(await item.element.itemTitle.textContent());
+  console.log(await item.element.itemDescription.textContent());
+  console.log(await item.element.itemPrice.textContent());
+
+  const itemByIndex = inventoryItemsPage.getInventoryItemByIndex(1);
+  console.log(await itemByIndex.element.itemTitle.textContent());
+  console.log(await itemByIndex.element.itemDescription.textContent());
+  console.log(await itemByIndex.element.itemPrice.textContent());
 });
